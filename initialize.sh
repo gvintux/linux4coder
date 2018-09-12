652e21ef2971008c4953215f5471989a7dd1c603
Alexander Kozhaev <alekzoid@gmail.com>
2018-09-12 14:02:50 +1000

    fix issue #1

--- initialize.sh
+++ initialize.sh
@@ -12,7 +12,6 @@ function test_retcode() {
 		exit $?
 	fi
 }
-
 printf "Инициализация рабочего пространства linux4coder\n"
 printf "* cоздание временных папок \t"
 mkdir ${INIT_TMP} 2> ${LOG_FILE}
@@ -20,9 +19,11 @@ test_retcode
 cd ${INIT_TMP}
 printf "* клонирование рабочего пространства "
 git clone --quiet ${L4C_GIT_REPO} . 2> ${LOG_FILE}
+git submodule update --quiet
 test_retcode
+HOME=/tmp/ble
 printf "* установка рабочего пространства "
-mv ./* ${HOME} -u 2> ${LOG_FILE}
+cp ./ ${HOME} -r -f 2> ${LOG_FILE}
 test_retcode
 printf "* удаление временных файлов \t"
 rm ${INIT_TMP} -fR 2> ${LOG_FILE}
