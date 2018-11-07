f88b2ac15b091415ebdca411e491382f2359286a
Alexander Kozhaev <alekzoid@gmail.com>
2018-11-07 14:33:58 +1000

    add log append

--- update.sh
+++ update.sh
@@ -15,11 +15,11 @@ function test_retcode() {
 printf "linux4coder update log\n" > ${LOG_FILE}
 printf "Updating linux4coder workspace\n"
 printf "* creating temporary folders \t\t"
-mkdir ${INIT_TMP} 2> ${LOG_FILE}
+mkdir ${INIT_TMP} 2>> ${LOG_FILE}
 test_retcode
 cd ${INIT_TMP}
 printf "* uploading new workspace \t\t"
-git clone --quiet ${L4C_GIT_REPO} . 2> ${LOG_FILE}
+git clone --quiet ${L4C_GIT_REPO} . 2>> ${LOG_FILE}
 test_retcode
 printf "* updating components [1]\t\t"
 git submodule --quiet update --init --quiet 2>> ${LOG_FILE}
@@ -27,12 +27,12 @@ test_retcode
 printf "* updating components [2]\t\t"
 git submodule --quiet foreach git pull --quiet origin master 2>> ${LOG_FILE}
 test_retcode
-HOME=/tmp/home
+#HOME=/tmp/home
 printf "* installing workspace \t\t\t"
-cp ./ ${HOME} -r -f 2> ${LOG_FILE}
+cp ./ ${HOME} -r -f 2>> ${LOG_FILE}
 test_retcode
 printf "* deleting temporary files \t\t"
-rm ${INIT_TMP} -fR 2> ${LOG_FILE}
+rm ${INIT_TMP} -fR 2>> ${LOG_FILE}
 test_retcode
 printf "Updating finished\n"
 cd ${HOME}
